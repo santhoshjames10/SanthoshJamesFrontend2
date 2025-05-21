16ecc9be93f0d1d21cfdb7e0d7acaf85e0d039c2
santhoshjames10 <santhoshjameslds@gmail.com>
2025-05-20 22:00:58 -1000

    commit tutorial with till lesson 2 we have created a list

---
+++ frontend2/src/Message.tsx
@@ -0,0 +1,11 @@
+function Message (){
+
+    //JSX: JavaScript XML
+    const name = "James"
+    if (name)
+        return <h1> Hello {name}</h1>;
+    return <h1> Hello World</h1>;
+}
+
+
+export default Message;
