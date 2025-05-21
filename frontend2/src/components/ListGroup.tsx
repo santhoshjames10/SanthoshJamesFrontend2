7134db7c6531a7fcfb28a25a8879ffb82631f4e3
santhoshjames10 <santhoshjameslds@gmail.com>
2025-05-20 22:23:31 -1000

    learning conditional Rendering

--- frontend2/src/components/ListGroup.tsx
+++ frontend2/src/components/ListGroup.tsx
@@ -1,15 +1,32 @@
 
 
 function ListGroup (){
+    let items = [
+        'New York',
+        'San Francisco',
+        'Los Angeles',
+        'Chicago',
+        'Houston',
+    ];
+
+    items = [];
+
+
+    const message = items.length === 0 ? 'No items found' : null;
+
+
+    
+
+    
     return (
         <>
             <h1> List</h1>
+            {items.length === 0 && <p> No items found</p>}
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                
+                {items.map((item) => (
+                    <li key ={item}>{item} </li>
+                ))}
             </ul>
         </>
     );
